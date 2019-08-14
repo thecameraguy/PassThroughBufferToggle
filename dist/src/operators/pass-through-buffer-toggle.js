c7da2eebac4b628a62fec5bfb41136e82c6b3e0f
JIN Choi <jinchoi@ca.ibm.com>
2019-08-14 18:14:53 -0400

    Proper dist and npm module

---
+++ dist/src/operators/pass-through-buffer-toggle.js
@@ -0,0 +1,142 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var Subscription_1 = require("rxjs/Subscription");
+var OuterSubscriber_1 = require("rxjs/OuterSubscriber");
+var subscribeToResult_1 = require("rxjs/util/subscribeToResult");
+/**
+ * A pass-through toggle-able buffer.
+ *
+ * Works like bufferToggle operator but when there is no buffer open, values pass straight
+ * through this operator and are emitted as an array of length 1.
+ *
+ * This is useful for using it as a bufferedGate. Open the gate to allow values to flow through,
+ * close the gate to buffer up the values until the gate opens next time. When the gate opens,
+ * buffered up values are emitted as an array and then new values are emitted as an array of
+ * length 1 as soon as they arrive.
+ *
+ * It is possible to generate multiple buffers. Multiple buffers behave exactly like bufferToggle.
+ *
+ * The author took a heavy inspiration from bufferToggle.ts in RxJS source. It's actually mostly
+ * bufferToggle with some minor modifications. We will be contributing this to RxJS project
+ * if they want to take it and at the very least will be publishing this operator to npm.
+ */
+function passThroughBufferToggle(openings, closingSelector) {
+    return this.lift(new PassThroughBufferToggleOperator(openings, closingSelector));
+}
+exports.passThroughBufferToggle = passThroughBufferToggle;
+var PassThroughBufferToggleOperator = /** @class */ (function () {
+    function PassThroughBufferToggleOperator(openings, closingSelector) {
+        this.openings = openings;
+        this.closingSelector = closingSelector;
+    }
+    PassThroughBufferToggleOperator.prototype.call = function (subscriber, source) {
+        return source.subscribe(new PassThroughBufferToggleSubscriber(subscriber, this.openings, this.closingSelector));
+    };
+    return PassThroughBufferToggleOperator;
+}());
+var PassThroughBufferToggleSubscriber = /** @class */ (function (_super) {
+    __extends(PassThroughBufferToggleSubscriber, _super);
+    function PassThroughBufferToggleSubscriber(destination, openings, closingSelector) {
+        var _this = _super.call(this, destination) || this;
+        _this.destination = destination;
+        _this.openings = openings;
+        _this.closingSelector = closingSelector;
+        _this.contexts = [];
+        _this.add(subscribeToResult_1.subscribeToResult(_this, openings));
+        return _this;
+    }
+    PassThroughBufferToggleSubscriber.prototype.notifyNext = function (outerValue, innerValue, outerIndex, innerIndex, innerSubscriber) {
+        outerValue ? this.closeBuffer(outerValue) : this.openBuffer(innerValue);
+    };
+    PassThroughBufferToggleSubscriber.prototype.notifyComplete = function (innerSubscriber) {
+        this.closeBuffer(innerSubscriber.context);
+    };
+    PassThroughBufferToggleSubscriber.prototype._next = function (value) {
+        var contexts = this.contexts;
+        var len = contexts.length;
+        // Any buffers open? Start collecting the values;
+        if (len > 0) {
+            for (var i = 0; i < len; ++i) {
+                contexts[i].buffer.push(value);
+            }
+            // All buffers are closed. Let the values through.
+        }
+        else {
+            this.destination.next([value]);
+        }
+    };
+    PassThroughBufferToggleSubscriber.prototype._error = function (err) {
+        var contexts = this.contexts;
+        while (contexts.length > 0) {
+            var context = contexts.shift();
+            this.clearContext(context);
+        }
+        this.contexts = null;
+        _super.prototype._error.call(this, err);
+    };
+    PassThroughBufferToggleSubscriber.prototype._complete = function () {
+        var contexts = this.contexts;
+        while (contexts.length > 0) {
+            var context = contexts.shift();
+            this.destination.next(context.buffer);
+            this.clearContext(context);
+        }
+        this.contexts = null;
+        _super.prototype._complete.call(this);
+    };
+    PassThroughBufferToggleSubscriber.prototype.clearContext = function (context) {
+        context.subscription.unsubscribe();
+        context.buffer = null;
+        context.subscription = null;
+    };
+    PassThroughBufferToggleSubscriber.prototype.openBuffer = function (value) {
+        try {
+            var closingSelector = this.closingSelector;
+            var closingNotifier = closingSelector.call(this, value);
+            if (closingNotifier) {
+                this.trySubscribe(closingNotifier);
+            }
+        }
+        catch (error) {
+            this._error(error);
+        }
+    };
+    PassThroughBufferToggleSubscriber.prototype.closeBuffer = function (context) {
+        var contexts = this.contexts;
+        if (contexts && context) {
+            var buffer = context.buffer, subscription = context.subscription;
+            this.destination.next(buffer);
+            contexts.splice(contexts.indexOf(context), 1);
+            this.remove(subscription);
+            subscription.unsubscribe();
+        }
+    };
+    PassThroughBufferToggleSubscriber.prototype.trySubscribe = function (closingNotifier) {
+        var contexts = this.contexts;
+        var buffer = [];
+        var subscription = new Subscription_1.Subscription();
+        var context = { buffer: buffer, subscription: subscription };
+        contexts.push(context);
+        var innerSubscription = subscribeToResult_1.subscribeToResult(this, closingNotifier, context);
+        if (!innerSubscription || innerSubscription.closed) {
+            this.closeBuffer(context);
+        }
+        else {
+            innerSubscription.context = context;
+            this.add(innerSubscription);
+            subscription.add(innerSubscription);
+        }
+    };
+    return PassThroughBufferToggleSubscriber;
+}(OuterSubscriber_1.OuterSubscriber));
+//# sourceMappingURL=pass-through-buffer-toggle.js.map
---
+++ dist/src/operators/pass-through-buffer-toggle.spec.js
@@ -0,0 +1,132 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("jasmine");
+var Rx_1 = require("rxjs/Rx");
+var pass_through_buffer_toggle_1 = require("./pass-through-buffer-toggle");
+// Patch Observable with the new operator
+Rx_1.Observable.prototype.passThroughBufferToggle = pass_through_buffer_toggle_1.passThroughBufferToggle;
+Rx_1.Observable.prototype._passThroughBufferToggle = pass_through_buffer_toggle_1.passThroughBufferToggle;
+describe('passThroughBufferToggle', function () {
+    var inputStream$;
+    var openings$;
+    var closings$;
+    var closingSelector;
+    beforeEach(function () {
+        jasmine.clock().install();
+        inputStream$ = new Rx_1.Subject();
+        openings$ = new Rx_1.Subject();
+        closings$ = new Rx_1.Subject();
+        closingSelector = createClosingSelector(closings$);
+    });
+    afterEach(function () {
+        jasmine.clock().uninstall();
+    });
+    it('should emit values immediately when no buffer is open', function () {
+        var emitted = [];
+        inputStream$
+            .passThroughBufferToggle(openings$, closingSelector)
+            .subscribe(function (value) {
+            emitted = emitted.concat(value);
+        });
+        inputStream$.next(1);
+        inputStream$.next(2);
+        inputStream$.next(3);
+        inputStream$.next(4);
+        jasmine.clock().tick(1000);
+        expect(emitted.length).toBe(4);
+        expect(emitted).toEqual([1, 2, 3, 4]);
+    });
+    it('should not emit values when one buffer is open', function () {
+        var emitted = [];
+        inputStream$
+            .passThroughBufferToggle(openings$, closingSelector)
+            .subscribe(function (value) {
+            emitted = emitted.concat(value);
+        });
+        openings$.next(true);
+        inputStream$.next(1);
+        inputStream$.next(2);
+        inputStream$.next(3);
+        inputStream$.next(4);
+        jasmine.clock().tick(1000);
+        expect(emitted.length).toBe(0);
+    });
+    it('should not emit values when more than one buffer is open', function () {
+        var emitted = [];
+        inputStream$
+            .passThroughBufferToggle(openings$, closingSelector)
+            .subscribe(function (value) {
+            emitted = emitted.concat(value);
+        });
+        openings$.next(true);
+        inputStream$.next(1);
+        inputStream$.next(2);
+        inputStream$.next(3);
+        inputStream$.next(4);
+        openings$.next(true);
+        inputStream$.next(5);
+        inputStream$.next(6);
+        inputStream$.next(7);
+        inputStream$.next(8);
+        jasmine.clock().tick(1000);
+        expect(emitted.length).toBe(0);
+    });
+    it('should emit all buffered values when buffers are closed', function () {
+        var emitted = [];
+        inputStream$
+            .passThroughBufferToggle(openings$, closingSelector)
+            .subscribe(function (value) {
+            emitted = emitted.concat(value);
+        });
+        openings$.next(true);
+        inputStream$.next(1);
+        inputStream$.next(2);
+        inputStream$.next(3);
+        inputStream$.next(4);
+        openings$.next(true);
+        inputStream$.next(5);
+        inputStream$.next(6);
+        inputStream$.next(7);
+        inputStream$.next(8);
+        closings$.next(true);
+        jasmine.clock().tick(1000);
+        expect(emitted.length).toBe(12);
+        expect(emitted).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 5, 6, 7, 8]);
+    });
+    it('should continue emitting values when buffers are closed', function () {
+        var emitted = [];
+        inputStream$
+            .passThroughBufferToggle(openings$, closingSelector)
+            .subscribe(function (value) {
+            emitted = emitted.concat(value);
+        });
+        openings$.next(true);
+        inputStream$.next(1);
+        inputStream$.next(2);
+        inputStream$.next(3);
+        inputStream$.next(4);
+        openings$.next(true);
+        inputStream$.next(5);
+        inputStream$.next(6);
+        inputStream$.next(7);
+        inputStream$.next(8);
+        closings$.next(true);
+        jasmine.clock().tick(1000);
+        expect(emitted.length).toBe(12);
+        expect(emitted).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 5, 6, 7, 8]);
+        emitted = [];
+        inputStream$.next(9);
+        inputStream$.next(10);
+        inputStream$.next(11);
+        inputStream$.next(12);
+        expect(emitted.length).toBe(4);
+        expect(emitted).toEqual([9, 10, 11, 12]);
+    });
+});
+var createClosingSelector = function (subject) {
+    var selector = function (value) {
+        return subject;
+    };
+    return selector;
+};
+//# sourceMappingURL=pass-through-buffer-toggle.spec.js.map
