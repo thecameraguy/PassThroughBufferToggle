13ece6fee6a3dcc68e15bab8671ce98487e228d7
JIN Choi <jinchoi@ca.ibm.com>
2019-08-15 14:41:37 -0400

    Clean up new rxjs6.x compatible code

--- src/operators/pass-through-buffer-toggle.ts
+++ src/operators/pass-through-buffer-toggle.ts
@@ -25,135 +25,131 @@ import { OperatorFunction, SubscribableOrPromise } from 'rxjs/internal/types';
  * if they want to take it and at the very least will be publishing this operator to npm.
  */
 export function passThroughBufferToggle<T, O>(
-  openings: SubscribableOrPromise<O>,
-  closingSelector: (value: O) => SubscribableOrPromise<any>
+    openings: SubscribableOrPromise<O>,
+    closingSelector: (value: O) => SubscribableOrPromise<any>
 ): OperatorFunction<T, T[]> {
-  return function bufferToggleOperatorFunction(source: Observable<T>) {
-    return source.lift(new BufferToggleOperator<T, O>(openings, closingSelector));
-  };
+
+    return function passThroughBufferToggleOperatorFunction(source: Observable<T>) {
+        return source.lift(new PassThroughBufferToggleOperator<T, O>(openings, closingSelector));
+    };
 }
 
-class BufferToggleOperator<T, O> implements Operator<T, T[]> {
+class PassThroughBufferToggleOperator<T, O> implements Operator<T, T[]> {
 
-  constructor(private openings: SubscribableOrPromise<O>,
-              private closingSelector: (value: O) => SubscribableOrPromise<any>) {
-  }
+    constructor(private openings: SubscribableOrPromise<O>,
+        private closingSelector: (value: O) => SubscribableOrPromise<any>) {
+    }
 
-  call(subscriber: Subscriber<T[]>, source: any): any {
-    return source.subscribe(new BufferToggleSubscriber(subscriber, this.openings, this.closingSelector));
-  }
+    call(subscriber: Subscriber<T[]>, source: any): any {
+        return source.subscribe(new PassThroughBufferToggleSubscriber(subscriber, this.openings, this.closingSelector));
+    }
 }
 
 interface BufferContext<T> {
-  buffer: T[];
-  subscription: Subscription;
+    buffer: T[];
+    subscription: Subscription;
 }
 
-/**
- * We need this JSDoc comment for affecting ESDoc.
- * @ignore
- * @extends {Ignored}
- */
-class BufferToggleSubscriber<T, O> extends OuterSubscriber<T, O> {
-  private contexts: Array<BufferContext<T>> = [];
+class PassThroughBufferToggleSubscriber<T, O> extends OuterSubscriber<T, O> {
+    private contexts: Array<BufferContext<T>> = [];
 
-  constructor(destination: Subscriber<T[]>,
-              private openings: SubscribableOrPromise<O>,
-              private closingSelector: (value: O) => SubscribableOrPromise<any> | void) {
-    super(destination);
-    this.add(subscribeToResult(this, openings));
-  }
+    constructor(destination: Subscriber<T[]>,
+        private openings: SubscribableOrPromise<O>,
+        private closingSelector: (value: O) => SubscribableOrPromise<any> | void) {
+        super(destination);
+        this.add(subscribeToResult(this, openings));
+    }
 
-  protected _next(value: T): void {
-    const contexts = this.contexts;
-    const len = contexts.length;
-    if (len > 0) {
-        for (let i = 0; i < len; i++) {
-        contexts[i].buffer.push(value);
+    protected _next(value: T): void {
+        const contexts = this.contexts;
+        const len = contexts.length;
+        if (len > 0) {
+            for (let i = 0; i < len; i++) {
+                contexts[i].buffer.push(value);
+            }
+        } else {
+            this.destination.next([value]);
         }
-    } else {
-        this.destination.next([value]);
     }
-  }
 
-  protected _error(err: any): void {
-    const contexts = this.contexts;
-    while (contexts.length > 0) {
-      const context = contexts.shift();
-      context.subscription.unsubscribe();
-      context.buffer = null;
-      context.subscription = null;
+    protected _error(err: any): void {
+        const contexts = this.contexts;
+        while (contexts.length > 0) {
+            const context = contexts.shift();
+            context.subscription.unsubscribe();
+            context.buffer = null;
+            context.subscription = null;
+        }
+        this.contexts = null;
+        super._error(err);
     }
-    this.contexts = null;
-    super._error(err);
-  }
 
-  protected _complete(): void {
-    const contexts = this.contexts;
-    while (contexts.length > 0) {
-      const context = contexts.shift();
-      this.destination.next(context.buffer);
-      context.subscription.unsubscribe();
-      context.buffer = null;
-      context.subscription = null;
+    protected _complete(): void {
+        const contexts = this.contexts;
+        while (contexts.length > 0) {
+            const context = contexts.shift();
+            this.destination.next(context.buffer);
+            context.subscription.unsubscribe();
+            context.buffer = null;
+            context.subscription = null;
+        }
+        this.contexts = null;
+        super._complete();
     }
-    this.contexts = null;
-    super._complete();
-  }
 
-  notifyNext(outerValue: any, innerValue: O,
-             outerIndex: number, innerIndex: number,
-             innerSub: InnerSubscriber<T, O>): void {
-    outerValue ? this.closeBuffer(outerValue) : this.openBuffer(innerValue);
-  }
-
-  notifyComplete(innerSub: InnerSubscriber<T, O>): void {
-    this.closeBuffer((<any> innerSub).context);
-  }
-
-  private openBuffer(value: O): void {
-    try {
-      const closingSelector = this.closingSelector;
-      const closingNotifier = closingSelector.call(this, value);
-      if (closingNotifier) {
-        this.trySubscribe(closingNotifier);
-      }
-    } catch (err) {
-      this._error(err);
+    notifyNext(outerValue: any, innerValue: O,
+        outerIndex: number, innerIndex: number,
+        innerSub: InnerSubscriber<T, O>): void {
+        outerValue ? this.closeBuffer(outerValue) : this.openBuffer(innerValue);
     }
-  }
 
-  private closeBuffer(context: BufferContext<T>): void {
-    const contexts = this.contexts;
-
-    if (contexts && context) {
-      const { buffer, subscription } = context;
-      this.destination.next(buffer);
-      contexts.splice(contexts.indexOf(context), 1);
-      this.remove(subscription);
-      subscription.unsubscribe();
+    notifyComplete(innerSub: InnerSubscriber<T, O>): void {
+        this.closeBuffer((<any>innerSub).context);
     }
-  }
 
-  private trySubscribe(closingNotifier: any): void {
-    const contexts = this.contexts;
-
-    const buffer: Array<T> = [];
-    const subscription = new Subscription();
-    const context = { buffer, subscription };
-    contexts.push(context);
-
-    const innerSubscription = subscribeToResult(this, closingNotifier, <any>context);
-
-    if (!innerSubscription || innerSubscription.closed) {
-      this.closeBuffer(context);
-    } else {
-      (<any> innerSubscription).context = context;
-
-      this.add(innerSubscription);
-      subscription.add(innerSubscription);
+    private openBuffer(value: O): void {
+        try {
+            const closingSelector = this.closingSelector;
+            const closingNotifier = closingSelector.call(this, value);
+            if (closingNotifier) {
+                this.trySubscribe(closingNotifier);
+            }
+        } catch (err) {
+            this._error(err);
+        }
     }
-  }
+
+    private closeBuffer(context: BufferContext<T>): void {
+        const contexts = this.contexts;
+
+        if (contexts && context) {
+            const { buffer, subscription } = context;
+            this.destination.next(buffer);
+            contexts.splice(contexts.indexOf(context), 1);
+            this.remove(subscription);
+            subscription.unsubscribe();
+        }
+    }
+
+    private trySubscribe(closingNotifier: any): void {
+        const contexts = this.contexts;
+
+        const buffer: Array<T> = [];
+        const subscription = new Subscription();
+        const context = { buffer, subscription };
+        contexts.push(context);
+
+        const innerSubscription = subscribeToResult(this, closingNotifier, <any>context);
+
+        if (!innerSubscription || innerSubscription.closed) {
+            this.closeBuffer(context);
+        } else {
+            (<any>innerSubscription).context = context;
+
+            this.add(innerSubscription);
+            subscription.add(innerSubscription);
+        }
+    }
 }
 
-(<any> Observable).prototype.passThroughBufferToggle = passThroughBufferToggle;
+(<any>Observable).prototype.passThroughBufferToggle = passThroughBufferToggle;
